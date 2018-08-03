4f721a3261bf86b6f258dbd04177c31d213a608a
Fiona Young <fiona.young.15@ucl.ac.uk>
2018-08-03 15:27:01 +0100

    first stuff

---
+++ plant_care.py
@@ -0,0 +1,31 @@
+'''
+Plant care for /ficus benjimina/
+'''
+
+Class Plant():
+  
+  def __init__(self, name):
+
+    self.name = name
+    self.alive = True
+    self.happy = True
+  
+  def water():
+    
+
+  def keep_alive():
+    
+    if not top_soil_wet:
+      self.water()
+
+
+
+def main():
+  
+  my_plant = Plant('ficus_benjimina')
+
+  while my_plant.alive:
+    my_plant.keep_alive()
+
+if __name__ == '__main__':
+  main()
